c6e2933147b87da728a951f01500283375abd2d4
João Rebelo Pires <joaorgpires@gmail.com>
2017-10-26 12:21:05 +0100

    Adding sum of two long long ints

---
+++ code/sumLongLong.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long int lld;
+
+int main() {
+    lld num_a, num_b;
+
+    cin >> num_a >> num_b;
+
+    lld sum = num_a + num_b;
+
+    cout << "The result is: " << sum << endl;
+
+    return 0;
+}
