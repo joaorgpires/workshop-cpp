ed082b0bcca6d692ccad0dd6dbed70b749d698fe
João Rebelo Pires <joaorgpires@gmail.com>
2017-10-26 14:11:08 +0100

    Increasing up to 40 mult

--- code/floatPrinting.cpp
+++ code/floatPrinting.cpp
@@ -7,9 +7,9 @@
 using namespace std;
 
 int main() {
-    for (int i = 1; i <= 10; i++) {
+    for (int i = 1; i <= 40; i++) {
         cout << left << setw(2) << setfill(' ') << i << ": ";
-        cout << right << fixed << setw(15) << setprecision(12) << setfill(' ') << PI * i << endl;
+        cout << right << fixed << setw(16) << setprecision(12) << setfill(' ') << PI * i << endl;
     }
     
     return 0;
