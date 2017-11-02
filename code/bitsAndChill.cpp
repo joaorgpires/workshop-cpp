55fda228e24462b26886c13f9e18330aa06ddbe1
yat0 <brunoccast.gacc@gmail.com>
2017-11-02 15:05:27 +0000

    Add simple usage example of <bits/stdc++.h>

---
+++ code/bitsAndChill.cpp
@@ -0,0 +1,34 @@
+/*
+ * 
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+ *
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  vector<int> numbers;
+  set<int> seen;
+  
+  int num;
+  while(cin >> num)
+  {
+    if(seen.find(num) != seen.end())
+      continue;
+
+    numbers.push_back(num);
+    seen.insert(num);
+  }
+  
+  sort(numbers.begin(), numbers.end());
+
+  for(size_t i=0; i!=numbers.size(); ++i)
+    cout << numbers[i] << " ";
+  cout << endl;
+}
