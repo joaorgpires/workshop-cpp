f5827d5212e59ed4d9c5f416dbea931965a53d3c
yat0 <brunoccast.gacc@gmail.com>
2017-11-08 18:59:04 +0000

    add flyerCode.cpp

---
+++ code/flyerCode.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+void main() {
+
+  int data[3] = {9,11,2017};
+  int hora_incio[2] = {14,00};
+
+  string local("Lab 1 - dcc");
+
+  string mais_info("Workshop dado por: \n"
+  "Jõao Pires e Bruno Casteleiro");
+  
+  return 0;
+}
